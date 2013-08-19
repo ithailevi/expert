6591358cc46c6ea5ef29a63eb4b30a41c6659d13
Ithai Levi <ithai.levi@gmail.com>
2013-08-19 13:31:01 +0300

    test concept.any

--- test/expert.concept.js
+++ test/expert.concept.js
@@ -27,4 +27,16 @@ describe("Concept",function() {
       assert(domain.example(mammal).length===1);
    });
 
+   it("can return a random linked concept by relation id", function() {
+      var mammal = domain.Concept.create({id:"mammal"}); 
+      var dog = domain.Concept.create({id:"dog"}),
+          cat = domain.Concept.create({id:"cat"}); 
+      dog.isa(mammal);
+      cat.isa(mammal);
+
+      var r = domain.$("mammal").any('example');
+
+      assert(r === dog || r === cat);
+      
+   });
 });
